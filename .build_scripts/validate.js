c2ff92ee4b120de9dc665045cc7ce012bc517e23
Marc Farra <marcfarra@gmail.com>
2016-08-23 17:39:31 -0400

    Add test for id uniqueness

--- .build_scripts/validate.js
+++ .build_scripts/validate.js
@@ -2,18 +2,34 @@ const ava = require('ava');
 const fs = require('fs');
 const path = require('path');
 const validate = require('../validator');
+const toml = require('toml');
 
 const files = fs.readdirSync('../datasets/');
 const datasets = files.filter((file) => file.endsWith('.toml'));
 const numDatasets = datasets.length;
 
+const data = datasets.map(function (dataset) {
+  return fs.readFileSync(path.join('../datasets', dataset)).toString();
+})
+
 ava.test('All valid datasets', t => {
   t.plan(numDatasets);
   console.log(`Validating ${numDatasets} datasets`);
 
-  datasets.forEach(function (dataset) {
-    const data = fs.readFileSync(path.join('../datasets', dataset)).toString();
+  data.forEach(function (item) {
+    t.true(validate(item), 'Dataset metadata should conform to specification');
+  });
+});
 
-    t.true(validate(data));
+ava.test('All unique dataset names', t => {
+  t.plan(numDatasets);
+
+  var names = new Set();
+
+  data.forEach(function (item) {
+    const parsedItem = toml.parse(item);
+    const itemName = parsedItem.name;
+    t.false(names.has(itemName), 'Dataset id should be unique');
+    names.add(itemName);
   });
 });
